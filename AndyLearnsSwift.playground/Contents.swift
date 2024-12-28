3ebfd6bf50438c0f0949ebeae5c252ec210eea95
doanah2000 <doanah1@uci.edu>
2024-12-28 12:46:00 -0800

    Day 2: Joining Strings

--- AndyLearnsSwift.playground/Contents.swift
+++ AndyLearnsSwift.playground/Contents.swift
@@ -1,27 +1,28 @@
 import Cocoa
 
-// Boolean snuck in because we've seen them in function calls
-let filename = "paris.jpg"
-print(filename.hasSuffix("jpg"))
+// Swift lets us join strings using "+" and string interpolation
 
-let number = 120
-print(number.isMultiple(of: 5))
+let hello = "Hello, "
+let world = "World!"
+let message = hello + world
+print(message)
 
-// Initialize boolean directly
-let goodDogs = true
-let gameOver = false
-let isMultiple = 120.isMultiple(of: 5)
+// Can join string literals in a similar manner
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
 
-// Booleans do not have arithmetic operations, but do have not operator (!)
-var isAuthenticated = false
-isAuthenticated = !isAuthenticated // isAuthenticated now true
-print(isAuthenticated)
-isAuthenticated = !isAuthenticated // isAuthhenticated back to false
-print(isAuthenticated)
+// + Being used to concat strings vs adding integers at the same time is called operator overloading, works
+// well ffor small things, but we wouldn't want to overload the usage (haha)
+// If we have a lot of things to join together, it becomes wasteful.. Swift cannot do all operations at
+// the same time, so the code below does 9 operations serially.
 
-// Booleans also have toggle function withh the same functionality as above example
-var isAlive = true
-isAlive.toggle()
-print(isAlive) // No longer alive
+let luggageCode = "1" + "2" + "3" + "4" + "5" + "6" + "7" + "8" + "9" + "0"
 
+// String interpolation let's us sufficiently create strings from other strings and integers, doubles, etc.
 
+let name = "Taylor"
+let age = 29
+let greeting = "Hello, my name is \(name), and I am \(age) years old!"
+print(greeting)
