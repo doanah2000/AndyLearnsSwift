4369e2ae5b2dc608b3376ebb772331324fb364aa
doanah2000 <doanah1@uci.edu>
2024-12-28 12:35:04 -0800

    Day 2: Booleans

--- AndyLearnsSwift.playground/Contents.swift
+++ AndyLearnsSwift.playground/Contents.swift
@@ -1,34 +1,27 @@
 import Cocoa
 
-// Swift considers floating point numbers as doubles. Thhere is a bit of inaccuracy in doubles
-let number = 0.1 + 0.2
-print(number)
+// Boolean snuck in because we've seen them in function calls
+let filename = "paris.jpg"
+print(filename.hasSuffix("jpg"))
 
-// Cannot mix ints and doubles. Must explicitly treat either the int as a double or double as an int
-let a = 1
-let b = 2.0
-let c = a + Int(b)
+let number = 120
+print(number.isMultiple(of: 5))
 
-let d = 1
-let e = 2.0
-let f = Double(d) + e
+// Initialize boolean directly
+let goodDogs = true
+let gameOver = false
+let isMultiple = 120.isMultiple(of: 5)
 
-let double1 = 3.1
-let double2 = 3131.3131
-let doublle3 = 3.0
-let int1 = 3
+// Booleans do not have arithmetic operations, but do have not operator (!)
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated // isAuthenticated now true
+print(isAuthenticated)
+isAuthenticated = !isAuthenticated // isAuthhenticated back to false
+print(isAuthenticated)
 
-// Once Swift decides on the data type of a constant or variable, it must be the same as long as it exists
-var name = "Nicolas Cage"
-name = "John travolta"
-//name = 57 - cannot do this
+// Booleans also have toggle function withh the same functionality as above example
+var isAlive = true
+isAlive.toggle()
+print(isAlive) // No longer alive
 
-// Doubles have the same compound assignment functionality, putting numbers here imply they are doubles
-var rating = 5.0
-rating *= 2
-rating /= 3
-rating += 4
-rating -= 5
-
-// Swift lets us use doubles and CGFloats interchangeably
 
