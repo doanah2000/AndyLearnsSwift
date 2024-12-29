ab087953f3dfa6ac6ad3181168f60d9e53d9e648
doanah2000 <doanah1@uci.edu>
2024-12-29 12:43:49 -0800

    Day 3: Intro to Arrays

--- AndyLearnsSwift.playground/Contents.swift
+++ AndyLearnsSwift.playground/Contents.swift
@@ -1,14 +1,53 @@
 import Cocoa
 
-// Checkpoint 1: Celsius to Fahrenheit Conversion
+/// In Swift, we store ordered data in arrays
+var beatles: Array<String> = ["John", "Paul", "George", " Ringo"]
+let numbers: Array<Int> = [1, 2, 3, 4, 5]
+var temperatures: Array<Double> = [37.0, 38.0, 39.0, 40.0, 41.0]
 
-///
-/// Goal:
-///     1. Create a constant holding any temperatur in Celsius
-///     2. Converts it to Fahrenheit by multiplying by 9, dividing by 5, then adding 32
-///     3. Prints the result ffor the user, showing both the Celsius and Fahrenheit values
-///
+/// We ask for data by index (0-index), make sure the index always exists
+print(beatles[0])
+print(numbers[1])
 
-let temperatureInCelsius: Double = 25.0
-let temperatureInFahrenheit: Double = temperatureInCelsius * 9 / 5.0 + 32
-let message:String = "The conversion for \(temperatureInCelsius)°C to Fahrenheit is \(temperatureInFahrenheit)°F."
+/// If array is variable, it can be modified after. Arrays can only store data off uniform type (type safety)
+beatles.append("Liverpool")
+// beatles.append(2) will not work
+
+/// We can specify array type on initialization
+var scores = Array<Int>()
+scores.append(100)
+scores.append(80)
+scores.append(90)
+print(scores[1])
+
+var albums: Array<String> = Array<String>()
+albums.append("Folklore")
+albums.append("Fearless")
+albums.append("Red")
+
+/// Can initialize arrays with [], but type must be explicit
+//var artists = [] not allowed
+var artists: Array<String> = []
+var artists2 = [String]()
+var artists3 = ["John"]
+
+/// Can use count to count number of items in array
+print(albums.count)
+
+/// .remove(at: <index>)
+albums.remove(at: 1)
+print(albums.count)
+
+/// .removeAll
+albums.removeAll()
+print(albums.count)
+
+/// .contains
+print(beatles.contains("John"))
+
+/// .sorted: for strings it will be alphanumerically, ints will be ascending order
+print(beatles.sorted())
+
+/// .reversed will reverse the array, but identify the collection as reversed
+print(beatles.reversed())
+
