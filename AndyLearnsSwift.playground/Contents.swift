b411444d0a5f3329051999d41f72e1d4e5ae86d6
doanah2000 <doanah1@uci.edu>
2024-12-29 13:20:43 -0800

    Day 3: Intro to Sets

--- AndyLearnsSwift.playground/Contents.swift
+++ AndyLearnsSwift.playground/Contents.swift
@@ -1,60 +1,28 @@
 import Cocoa
 
-/// Reading items from index can be problematic and dangerous, since fields would not be explicit and if arrays are variable the index may no longer be valid. Solution to this is Dictionaries
-let employee = [
-    "name": "Taylor Swift",
-    "job": "Singer",
-    "location": "Nashville"
+/// Sets work similarly to arrays except they are unordered and items cannot be duplicated
+var actors: Set<String> = [
+    "Denzel Washington",
+    "Tom Cruise",
+    "Brad Pitt",
+    "Leonardo DiCaprio",
+    "Will Smith",
 ]
-/// Fields on the left are keys, fields on right are values.
-print(employee["name"])
-print(employee["job"])
-print(employee["location"])
+print(actors)
 
-/// Key may not exist:
-print(employee["age"]) // prints "nil"
+var artists: Set<String> = Set([
+    "Beyoncé",
+    "Justin Timberlake",
+    "Ariana Grande",
+])
+print(artists)
 
-/// When you access data like this, the result may or may not be empty, similar to Java optionals
-/// When dealing with dictionaries, we can p[rovide a default value if keys do not exist
-print(employee["name", default: "Unknown"])
-print(employee["job", default: "Unknown"])
-print(employee["location", default: "Unknown"])
-print(employee["age", default: "Unknown"])
+/// .insert
+actors.insert("Tom Hanks")
+artists.insert("Rihanna")
+print(actors)
+print(artists)
 
-/// We can use other data types as much as possible
-let hasGraduated: [String: Bool] = [
-    "Tim": false,
-    "Jake": true,
-    "Taylor": true
-]
-
-let olympics: Dictionary<Int, String> = [
-    2012: "London",
-    2016: " Rio",
-    2020: "Tokyo"
-]
-
-print(olympics[2012, default: "Unknown"])
-
-/// Can initialize empty dictionary and "put" key--value pairs
-var heights = [String: Int]()
-heights["Tim"] = 178
-heights["Jake"] = 180
-heights["Taylor"] = 179
-print(heights)
-
-/// Dictionaries do not allow duplicate keys. If key exists, value is overwritten
-print(heights["Tim", default: 0])
-heights["Tim"] = 181
-print(heights["Tim", default: 0])
-
-/// .count
-print(heights.count)
-
-/// .removeValue
-heights.removeValue(forKey: "Tim")
-print(heights["Tim", default: 0])
-
-/// .removeAll
-heights.removeAll()
-print(heights.count)
+/// Sets have no order, but calling .contains is much more optimized for sets than for arrays. When running .sorted, returns a sorted array.
+let sortedActors: Array<String> = actors.sorted()
+print(sortedActors)
