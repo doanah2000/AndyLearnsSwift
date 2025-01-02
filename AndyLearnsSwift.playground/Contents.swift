ee1e5b77432c0131cef219a1d00a332794a97d75
doanah2000 <doanah1@uci.edu>
2025-01-01 19:14:53 -0800

    Day 5: Switch Statements

--- AndyLearnsSwift.playground/Contents.swift
+++ AndyLearnsSwift.playground/Contents.swift
@@ -1,19 +1,45 @@
 import Cocoa
 
-/// If Statements in Swifft are virtually the same as other languages. Note comparisons can only occur for matching data types. Combine conditions with && and ||
-var someCondition: Bool = true
+/// Using purely if-else if statements can get a bit messy. It can be repetitive and error prone due to lack of readability. To help in the condition of checking one variable, we can use switch statements.
+/// This allows us to check individual conditions, and Swift can help us out. If one condition is checked twice, Swift will complain. If one condition is forgotten, Swift will not build.
 
-if someCondition {
-  print("Do something")
+enum Weather {
+    case sun, rain, wind, snow, unknown
 }
 
+let forecast: Weather = .sun
 
-/// If - else if - else
-let score = 85
-if score >= 90 {
-    print("You got an A")
-} else if score >= 80 {
-    print("You got an B")
-} else {
-    print("You did not get an A nor B")
+switch forecast {
+case .sun:
+    print("It's sunny!")
+case .rain:
+    print("It's raining!")
+case .wind:
+    print("It's windy!")
+case .snow:
+    print("It's snowing!")
+default:
+    print("Weather is broken!")
+}
+
+/// Note: Default block must be at the end, as Swift executes cases in order. If we want Swift to keep going after the first case, use fall through
+
+let day = 5
+print("My true love gave to me...")
+
+switch day {
+case 5:
+    print("5 golden rings")
+    fallthrough
+case 4:
+    print("4 calling birds")
+    fallthrough
+case 3:
+    print("3 French hens")
+    fallthrough
+case 2:
+    print("2 turtle doves")
+    fallthrough
+default:
+    print("A partridge in a pear tree")
 }
