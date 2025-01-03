f02779c577d5163b0c14974bad2c3532ee8a6ce5
doanah2000 <doanah1@uci.edu>
2025-01-03 09:14:16 -0800

    Day 7: Functions

--- AndyLearnsSwift.playground/Contents.swift
+++ AndyLearnsSwift.playground/Contents.swift
@@ -1,22 +1,20 @@
 import Cocoa
 
-/// Checkpoint 3: FizzBuzz: Loop through each number from 1 through 100 and
-/// 1. If it's a multiple of 3, print Fizz
-/// 2. If it's a multiple of 5, print Buzz
-/// 3. If it's a multiple of 3 and 5, print FizzBuzz
-/// 4. Otherwise, just print the number
+/// Functions are chunks of code carved from the main program then given a name to be referred elsewhere
 
-for number in 1...100 {
-    if number.isMultiple(of: 3) && number.isMultiple(of: 5) {
-        print("FizzBuzz")
-    }
-    else if number.isMultiple(of: 3) {
-        print("Fizz")
-    }
-    else if number.isMultiple(of: 5) {
-        print("Buzz")
-    }
-    else {
-        print(number)
+func showWelcome() {
+    print("Welcome to my app!")
+    print("By default, this prints out a conversion")
+    print("chart from centimeters to inches, but you")
+    print("can also set a custom range if you want.")
+}
+
+showWelcome() // This is known as the function call, or callsite
+
+func printTimesTables(number: Int) {
+    for i in 1...12 {
+        print("\(i) * \(number) = \(i * number)")
     }
 }
+
+printTimesTables(number: 2)
