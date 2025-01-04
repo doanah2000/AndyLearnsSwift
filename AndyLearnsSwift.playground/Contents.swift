0a245ad840ef658f036fc73918d91424acb52fa6
doanah2000 <doanah1@uci.edu>
2025-01-04 15:30:57 -0800

    Day 8: Default values for parameters

--- AndyLearnsSwift.playground/Contents.swift
+++ AndyLearnsSwift.playground/Contents.swift
@@ -1,32 +1,11 @@
 import Cocoa
 
-/// Functions offten pass data back
+/// We can set parameter default values in the function definition. We can still specify end as a different number, but if we don''t then end will by default be 12.
 
-let root = sqrt(169) // 13 is now stored in root
-print(root)
-
-/// If we want to return our own value from a function, we'll need `-> type` before the open brace, and have the return keyword in the function body
-
-func rollDice() -> Int {
-    return Int.random(in: 1...6)
+func printTimesTables(for number: Int, end: Int = 12) {
+    for i in 1...end {
+        print("\(i) x \(number) = \(i * number)")
+    }
 }
 
-let result = rollDice()
-print(result)
-
-/// Do two strings contain the same letters, regardless of order?
-/// Function should:
-///     Accept two string parameters
-///     Return true if their letters are the same
-
-/// When our function only has one line, we can remove the return keyword entirely.
-func containsSameLetters(firstString: String, secondString: String) -> Bool {
-    firstString.sorted() == secondString.sorted()
-}
-
-print(containsSameLetters(firstString: "Hello", secondString: "World"))
-
-/// If our func has no return value, we can use return by itself, forcing a function to exit immediately
-func printStuff() {
-    return
-}
+printTimesTables(for: 5)
