5b04fce8e4b9de454fe4c0451cdffda32db6f5d6
doanah2000 <doanah1@uci.edu>
2025-01-04 17:57:49 -0800

    Checkpoint 4

--- AndyLearnsSwift.playground/Contents.swift
+++ AndyLearnsSwift.playground/Contents.swift
@@ -1,42 +1,32 @@
 import Cocoa
 
-/// Swift forces us to acknowledge that there may be errors that arise in our code
-/// 1. Define all errors that may occur from our code
-/// 2. Write a function that works like normal but can throw one or more errors if a serious error is found.
-/// 3. Try and run that function and handle any errors that come back
+/// Checkpoint 4: Write a function that accepts an integer from 1 through 10,000 and returns the integer square root of that number
+/// 1. You can't use the built-in sqrt() function or similar - you need to ffind the square root yourselff
+/// 2. If the number is less than 1 or greater than 10,000, you should throw an "out of bounds" error
+/// 3. You should only consider integer square roots - don't worry about a square root of 3 being 1.732, for example.
+/// 4. If you can't find the square root, throw a "no root" error
 
-// Step 1
-enum PasswordError: Error {
-    case short, obvious
+enum squareRootError: Error {
+    case outOfBounds, noRoot
 }
 
-// Step 2
-func checkPassword(_ password: String) throws -> String {
-    if password.count < 8 {
-        throw PasswordError.short
-    }
-    if password == "password" {
-        throw PasswordError.obvious
+func findSquareRoot(_ number: Int) throws -> Int {
+    if number < 1 || number > 10_000 {
+        throw squareRootError.outOfBounds
     }
     
-    if password.count < 8 {
-        return "OK"
-    } else if password.count < 10 {
-        return "Good"
-    } else {
-        return "Excellent"
+    for i in 1...100 {
+        if i * i == number {
+            return i
+        } else if i * i > number {
+            throw squareRootError.noRoot
+        }
     }
+    return number // dummy return, since the potential square root is thrown once an integer squared is greater than the given number
 }
 
-// Step 3
 do {
-    let result = try checkPassword("password")
-    print(result)
-} catch PasswordError.short {
-    print("too short")
-} catch PasswordError.obvious {
-    print("too obvious")
+    try print(findSquareRoot(100))
 } catch {
     print("Error: \(error)")
 }
-
