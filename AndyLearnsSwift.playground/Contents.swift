2c82f9c0c20e1483d9679cd039701e2cef58652b
doanah2000 <doanah1@uci.edu>
2025-01-06 08:24:39 -0800

    Checkpoint 5: Closures and passing functions

--- AndyLearnsSwift.playground/Contents.swift
+++ AndyLearnsSwift.playground/Contents.swift
@@ -1,64 +1,26 @@
 import Cocoa
 
-/// Writing functions that accept functions as parameters is particularly important closures thanks to trailing closure syntax
+/// Checkpooint 5:
+/// Your input is this:
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
 
-func greetUser() {
-    print("Hi there!")
-}
+/// Your job is to:
+/// - Filter out even numbers
+/// - Sort array in ascending order
+/// - Map them to strings in the format "7 is a lucky number"
+/// - Print each element on a separate line
+/// - Without using temporary variables
 
-greetUser()
-var greetCopy: () -> Void = greetUser
-greetCopy()
-
-/// We write a function that generates an array of integers by calling another passed in function.
-func makeArray(size: Int, using generator: () -> Int) -> [Int] {
-	var numbers = [Int]()
-	for _ in 0..<size {
-		numbers.append(generator())
+/// Solo function
+func printItemByLine(_ strarray: [String]) {
+	for item in strarray {
+		print(item)
 	}
-	return numbers
-}
-/// Ex 1
-let rolls = makeArray(size: 50) {
-	Int.random(in: 1...20)
-}
-print(rolls)
-
-/// Ex 2
-func generateNumber() -> Int {
-	Int.random(in: 1...20)
 }
 
-let newRolls = makeArray(size: 50, using: generateNumber)
-print(newRolls)
-
-/// Do we need to declare the function in the parameter instead of just calling another function directly
-
-func doSomething() {
-	print("Doing Something")
+func printSortedLuckyOddNumbers(_ numbers: [Int], _ printFunc: ([String]) -> Void) {
+	printFunc(numbers.filter{ !$0.isMultiple(of: 2) }.sorted().map {"\($0) is a lucky number"})
 }
 
-func doMoreStuff() {
-	doSomething()
-	print("Did something")
-}
+printSortedLuckyOddNumbers(luckyNumbers, printItemByLine)
 
-doMoreStuff()
-
-/// We can but this looks cleaner and groups reusability if we wanna do trailing closures
-func doImportantWork(first: () -> Void, second: () -> Void, third: () -> Void) {
-	print("About to start first work")
-	first()
-	print("About to start second work")
-	second()
-	print("About to start third work")
-	third()
-}
-
-doImportantWork {
-	print("This is the first work")
-} second: {
-	print("This is the second work")
-} third: {
-	print("This is the third work")
-}
