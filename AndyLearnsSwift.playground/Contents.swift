788e4c25d3d4c213e22ce15c3f37e5448d30f9c0
doanah2000 <doanah1@uci.edu>
2025-01-10 07:37:09 -0800

    Day 11: Static Methods

--- AndyLearnsSwift.playground/Contents.swift
+++ AndyLearnsSwift.playground/Contents.swift
@@ -35,3 +35,15 @@ print(newKid.getAge())
 /// Use fileprivate for "Don't let anything outside the current file use this"
 /// Use public for "Anyone can use this"
 /// Use private(set) to "let anyone read the property, but only let my methods write it"
+
+
+/// Sometimes we want to add a property or method to the struct itself, and not an instance of it. We can use the `static` keyword here
+struct School {
+	static let studentCount = 0
+
+	static func add(student: String) {
+		print("\(student) joined the school.")
+		studentCount += 1
+	}
+}
+
